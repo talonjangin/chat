691ccd031fc825931b7f3c29d3ac5094161afe3f
yoojisung0603 <64051466+yoojisung0603@users.noreply.github.com>
2020-06-27 00:38:07 +0900

    upload

---
+++ app.js
@@ -0,0 +1,12 @@
+const express = require("express")
+const app = express()
+
+const port = process.env.PORT || 8080
+
+app.get("/", function (req, res) {
+  res.send("hello")
+})
+app.listen(port, function () {
+  console.log("Server running at http://127.0.0.1:%s", port)
+  console.log("Server running at http://127.0.0.1:%s/ko/quiz/1", port)
+})
