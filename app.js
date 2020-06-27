5487c3636c203ed15b83cb12feeff6fc00d9eb12
yoojisung0603 <64051466+yoojisung0603@users.noreply.github.com>
2020-06-27 13:58:56 +0900

    upload

--- app.js
+++ app.js
@@ -7,13 +7,34 @@ const app = express()
 const server = http.createServer(app)
 const io = socket(server)
 
+// view engine
+app.set("views", __dirname + "/views")
+app.set("view engine", "ejs")
+app.engine("html", require("ejs").renderFile)
+app.use("/static", express.static(__dirname + "/public"))
+
+//rendering
+app.get("/chat", function (req, res) {
+  res.render("chat")
+})
 app.get("/", function (req, res) {
-  res.send("hello world good bye")
+  res.render("main")
+})
+
+//socket.io
+
+io.on("connection", socket => {
+  //console.log("io connected")
+
+  socket.emit("message", "welcome jschat")
+
+  socket.on("chatMessage", msg => {
+    io.emit("message", msg)
+  })
 })
 
 // server
 const port = process.env.PORT || 8080
 server.listen(port, function () {
   console.log("Server running at http://127.0.0.1:%s", port)
-  console.log("Server running at http://127.0.0.1:%s/ko/quiz/1", port)
 })
