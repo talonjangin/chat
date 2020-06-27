9934a1bc02ad2d6d8297bce035825ad9937a530d
yoojisung0603 <64051466+yoojisung0603@users.noreply.github.com>
2020-06-27 12:15:37 +0900

    upload

--- app.js
+++ app.js
@@ -1,12 +1,19 @@
 const express = require("express")
+const http = require("http")
+const path = require("path")
+const socket = require("socket.io")
 const app = express()
 
-const port = process.env.PORT || 8080
+const server = http.createServer(app)
+const io = socket(server)
 
 app.get("/", function (req, res) {
   res.send("hello world good bye")
 })
-app.listen(port, function () {
+
+// server
+const port = process.env.PORT || 8080
+server.listen(port, function () {
   console.log("Server running at http://127.0.0.1:%s", port)
   console.log("Server running at http://127.0.0.1:%s/ko/quiz/1", port)
 })
